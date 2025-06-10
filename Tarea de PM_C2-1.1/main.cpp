d0b2fd56bb5cb6c5df88340463bd7574ada91bd0
JanielGM <wanderjgm12@gmail.com>
2025-06-10 01:39:46 -0400

    Revert "TAREAS PM_C2"
    This reverts commit 187e1b88db61a7e024f906f5ff1705ce8a09a388.

---
+++ Tarea de PM_C2-1.1/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(void)
+{
+    int A, B, C, D;
+
+    printf("Ingrese cuatro datos de tipo entero: ");
+
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+
+    printf("\n%d %d %d %d\n", A, B, C, D);
+
+    return 0;
+}
+
