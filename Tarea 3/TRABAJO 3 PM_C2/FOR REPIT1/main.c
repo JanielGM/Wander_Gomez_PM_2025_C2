7cd27f6d02ca22cab52f717e71f27a40f36c0bd2
JanielGM <wanderjgm12@gmail.com>
2025-07-14 07:40:37 -0400

    Tarea 3
    Tarea para Meca

---
+++ Tarea 3/TRABAJO 3 PM_C2/FOR REPIT1/main.c
@@ -0,0 +1,39 @@
+ #include <stdio.h>
+ /* Nómina.
+ El programa, al recibir los salarios de 15 profesores, obtiene el total de la
+ ➥nómina de la universidad.
+
+ I: variable de tipo entero.
+ SAL y NOM: variables de tipo real. */
+ void main(void)
+ {
+ int I;
+ float SAL, NOM;
+ NOM = 0;
+ for (I=1; I<=4; I++)
+ {
+ printf("\nIngrese el salario del profesor%d:\t", I);
+ scanf("%f", &SAL);
+ NOM = NOM + SAL;
+ }
+ {
+     printf()
+ }
+ //imprimir el pago de los profesores
+ //por cada uno menos el 20%
+ //imprimir el pago de nomina
+ //imprimir el total a los profesores sin el impuesto
+ //imprimir el total de impuesto
+ printf(" salario original %.2f\n", SAL);
+ printf(" Impuesto (20%):  &.2f\n", IMPUESTO_INDIVIDUAL);
+ printf(" Pago neto al profesor %d: %.2f\n")
+ printf("\nEl total de la nomina es: %.2f", NOM);
+
+ //imprimir resultados finales
+
+ printf("\n--- resumen de la Nomina ---\n");
+ printf("El total de la nomina bruta (sin impuestos) es: %.2f\n", NOW);
+ printf("El total a pagar a los profesores (ya con impuestos descontado) es: %.2f\n"),
+ printf("El total de impuesto retenido es: %.2f\n", TOTAL_IMPUESTO);
+ printf("\nFin de programa\n");
+ }
